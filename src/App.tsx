a0f0ca8bc763417898bcdf6a430f73d8ee431762
Andy Gonzalez <agonzalezjr@gmail.com>
2022-10-20 23:04:39 -0400

    switch app to use DataStore (fail)

--- src/App.tsx
+++ src/App.tsx
@@ -22,27 +22,34 @@ import {
 
 import { Storage } from 'aws-amplify';
 
+import { DataStore } from '@aws-amplify/datastore';
+import { Note } from './models';
+
 function App({ signOut }: WithAuthenticatorProps) {
-  const [notes, setNotes] = useState<Array<any>>([]);
+  const [notes, setNotes] = useState<Array<Note>>([]);
 
   useEffect(() => {
     fetchNotes();
   }, []);
 
   async function fetchNotes() {
-    const apiData: any = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
+    // With GraphQL API
+    // const apiData: any = await API.graphql({ query: listNotes });
+    // const notesFromAPI = apiData.data.listNotes.items;
+
+    // Through DataStore
+    const notesFromAPI = await DataStore.query(Note);
 
     // Load the storage links manually ...
-    await Promise.all(
-      notesFromAPI.map(async (note: any) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
+    // await Promise.all(
+    //   notesFromAPI.map(async (note: any) => {
+    //     if (note.image) {
+    //       const url = await Storage.get(note.name);
+    //       note.image = url;
+    //     }
+    //     return note;
+    //   })
+    // );
 
     setNotes(notesFromAPI);
   }
@@ -52,9 +59,9 @@ function App({ signOut }: WithAuthenticatorProps) {
     const form = new FormData(event.target);
     const image: any = form.get("image");
     const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name
+      name: form.get("name") as string,
+      description: form.get("description") as string,
+      image: image.name as string
     };
 
     // Save the image manually ...
@@ -62,10 +69,17 @@ function App({ signOut }: WithAuthenticatorProps) {
       await Storage.put(data.name as string, image);
     }
 
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
+    // await API.graphql({
+    //   query: createNoteMutation,
+    //   variables: { input: data },
+    // });
+
+    // Using DataStore
+    await DataStore.save(
+      new Note(data)
+    );
+
+    // do we need this??
     fetchNotes();
     event.target.reset();
   }
@@ -77,10 +91,16 @@ function App({ signOut }: WithAuthenticatorProps) {
     // Delete the image manually ...
     await Storage.remove(name);
 
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
+    // await API.graphql({
+    //   query: deleteNoteMutation,
+    //   variables: { input: { id } },
+    // });
+
+    // Using DataStore
+    const modelToDelete = await DataStore.query(Note, id);
+    if (modelToDelete) {
+      await DataStore.delete(modelToDelete);
+    }
   }
 
   return (
